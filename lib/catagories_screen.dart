a3710f2aec1d49c51783d7d2c803c73fbbbbab3b
Kalam031 <abukalam973@gmail.com>
2020-10-21 11:06:08 +0600

    registering a screen as the main screen

--- lib/catagories_screen.dart
+++ lib/catagories_screen.dart
@@ -5,20 +5,27 @@ import './category_item.dart';
 class CatergoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-          .map(
-            (catData) => CategoryItem(
-              catData.title,
-              catData.color,
-            ),
-          )
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        mainAxisSpacing: 20,
-        crossAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'DeliMeal',
+        ),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                catData.title,
+                catData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
       ),
     );
   }
